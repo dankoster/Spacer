6f49a2fbbe9b413e412b348daed77ee254bf4d8e
Dan Koster <koster.dan@gmail.com>
2019-01-05 01:22:37 -0800

    Test by adding objects on demand

--- Game.js
+++ Game.js
@@ -1,5 +1,3 @@
-// <reference path="SpaceObject.js" />
-
 import { SpaceObject } from './SpaceObject.js'
 import { Universe } from './Universe.js'
 import { Vector } from './Vector.js'
@@ -15,9 +13,9 @@ export function Game() {
   self.universe = new Universe();
 
   //TODO: dynamically add SVG objects from the JavaScript
-  var selectedObject = new SpaceObject('bluecircle', 5000000);
+  var selectedObject = new SpaceObject('bluecircle', 100000);
   self.universe.Add(selectedObject);
-  self.universe.Add(new SpaceObject('redcircle1', 5000));
+  //self.universe.Add(new SpaceObject('redcircle1', 1000));
 
   self.thrust = {
     U: new Vector({ X: 0, Y: -.2 }),
@@ -107,12 +105,32 @@ export function Game() {
     // self.update(tFrame);
     self.render(tFrame);
   }
-  
+
 }
 
-Game.prototype.test = function() {
-	alert('Hello game!')
+Game.prototype.test = function () {
+
+  function random(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  var id = Date.now()
+  //<circle cx="125.1" cy="80" r="20" fill="red" id="redcircle1" />
+
+  var svg = document.getElementsByTagName('svg')[0]
+  var newElement = document.createElementNS("http://www.w3.org/2000/svg", 'circle'); //Create a path in SVG's namespace
+  newElement.setAttribute("id", id)
+  newElement.setAttribute("cx", random(20,800))
+  newElement.setAttribute("cy", random(0,600))
+  newElement.setAttribute("r", 15)
+  newElement.setAttribute("fill", "red")
+  svg.appendChild(newElement)
+
+  var so = new SpaceObject(id, 50000)
+  this.universe.Add(so)
 }
+
+
 
 // Game.prototype.update = function(tFrame) {
 //   console.log(tFrame); 
@@ -127,13 +145,13 @@ Game.prototype.render = function (frame) {
   this.RenderVectors()
 }
 
-Game.prototype.RenderVectors = function() {
+Game.prototype.RenderVectors = function () {
   this.universe.Vectors.forEach(v => {
     if (!v.id) {
       v.id = Date.now()
       var svg = document.getElementsByTagName('svg')[0]
       var newElement = document.createElementNS("http://www.w3.org/2000/svg", 'line'); //Create a path in SVG's namespace
-      if(v.name) newElement.setAttribute("name", v.name);
+      if (v.name) newElement.setAttribute("name", v.name);
       newElement.setAttribute("id", v.id);
       newElement.setAttribute("x1", v.position.X);
       newElement.setAttribute("y1", v.position.Y);
@@ -145,11 +163,10 @@ Game.prototype.RenderVectors = function() {
       newElement.style.strokeWidth = "1px"; //Set stroke width
       svg.appendChild(newElement);
     }
-    else if(Date.now() - v.id > 5000) 
-    {
+    else if (Date.now() - v.id > 5000) {
       //remove old lines
       var line = document.getElementById(v.id)
-      if(line) {
+      if (line) {
         const index = this.universe.Vectors.indexOf(v)
         this.universe.Vectors.splice(index, 1)
         line.parentNode.removeChild(line)
--- main.js
+++
@@ -1,8 +0,0 @@
-import { Game } from './Game.js'
-
-(function() {
-    
-  var game = new Game();
-  game.mainLoop(); // Start the cycle
-  
-})();
